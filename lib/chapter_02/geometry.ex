c2f2d7f7b421188f13f443d9953b13edfd76fe3c
muzhawir <muzhawir.amri@gmail.com>
2023-12-19 02:41:44 +0800

    feat: add listing 2.1
    Listing 2.1. Defining a module (geometry.ex)

---
+++ lib/chapter_02/geometry.ex
@@ -0,0 +1,12 @@
+defmodule Chapter02.Geometry do
+  @moduledoc """
+  Chapter 2: Building blocks
+
+  2.3 Organizing your code
+    2.3.1 Modules
+      Listing 2.1. Defining a module (geometry.ex)
+  """
+  def rectangle_area(length, width) do
+    length * width
+  end
+end
