13a29dd68053e5abd3bb4604852685bc72307d82
muzhawir <muzhawir.amri@gmail.com>
2023-12-18 17:42:47 +0800

    Initial commit

---
+++ lib/learn_elixir_in_action.ex
@@ -0,0 +1,18 @@
+defmodule LearnElixirInAction do
+  @moduledoc """
+  Documentation for `LearnElixirInAction`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LearnElixirInAction.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/learn_elixir_in_action_test.exs
@@ -0,0 +1,8 @@
+defmodule LearnElixirInActionTest do
+  use ExUnit.Case
+  doctest LearnElixirInAction
+
+  test "greets the world" do
+    assert LearnElixirInAction.hello() == :world
+  end
+end
