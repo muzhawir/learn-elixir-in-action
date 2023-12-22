e7a9cfe7be66a222fc7b12e93eab86663e407635
muzhawir <muzhawir.amri@gmail.com>
2023-12-23 02:00:00 +0800

    fix: delete learn_elixir_in_action module and test

--- test/learn_elixir_in_action_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule LearnElixirInActionTest do
-  use ExUnit.Case
-
-  doctest LearnElixirInAction
-
-  test "greets the world" do
-    assert LearnElixirInAction.hello() == :world
-  end
-end
