7e3947699496288b78d4cc5b6e7e5f815e1b766a
muzhawir <muzhawir.amri@gmail.com>
2023-12-23 02:00:55 +0800

    feat: add listing 2.2
    Add listing 2.2. Functions with the same name but different arities

---
+++ test/chapter_02/arity_test.exs
@@ -0,0 +1,13 @@
+defmodule Chapter02.ArityTest do
+  use ExUnit.Case
+
+  import Chapter02.Arity
+
+  test "Successfully calculate rectangle_area/1" do
+    assert 100 === rectangle_area(10)
+  end
+
+  test "Successfully calculate rectangle_area/2" do
+    assert 400 === rectangle_area(20, 20)
+  end
+end
